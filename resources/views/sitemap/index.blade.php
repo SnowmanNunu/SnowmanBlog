bc412c34ebf76c489566d810d204dd155a2917ae
SnowmanNunu <345750542@qq.com>
2026-04-25 13:25:01 +0800

    auto update: 2026-04-25 13:25:01

---
+++ resources/views/sitemap/index.blade.php
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>{{ now()->toDateString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>{{ route('guestbook.index') }}</loc>
+        <lastmod>{{ now()->toDateString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    @foreach($categories as $category)
+    <url>
+        <loc>{{ route('blog.category', $category->slug) }}</loc>
+        <lastmod>{{ $category->updated_at->toDateString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    @endforeach
+    @foreach($tags as $tag)
+    <url>
+        <loc>{{ route('blog.tag', $tag->slug) }}</loc>
+        <lastmod>{{ $tag->updated_at->toDateString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    @endforeach
+    @foreach($posts as $post)
+    <url>
+        <loc>{{ route('blog.show', $post->slug) }}</loc>
+        <lastmod>{{ $post->updated_at->toDateString() }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+</urlset>
