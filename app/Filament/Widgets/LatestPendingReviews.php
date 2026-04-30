0f2873a9b9c5c5dc9cd49edb0cc8f42f22e48fb6
SnowmanNunu <345750542@qq.com>
2026-04-30 09:41:22 +0800

    fix: LatestPendingReviews 缺少 HasForms 接口导致模态框报错

--- app/Filament/Widgets/LatestPendingReviews.php
+++ app/Filament/Widgets/LatestPendingReviews.php
@@ -7,14 +7,17 @@ use App\Models\Guestbook;
 use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Notifications\Notification;
 use Filament\Support\Contracts\TranslatableContentDriver;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Facades\DB;
 
-class LatestPendingReviews extends Widget implements HasActions
+class LatestPendingReviews extends Widget implements HasActions, HasForms
 {
     use InteractsWithActions;
+    use InteractsWithForms;
 
     protected static string $view = 'filament.widgets.latest-pending-reviews';
 
